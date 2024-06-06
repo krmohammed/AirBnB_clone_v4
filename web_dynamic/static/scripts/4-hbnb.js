fbc253441e82b3a8cc34ba9c910dd625c754b7a4
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-06-06 13:40:15 +0000

    task 5, wrapping up

---
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -0,0 +1,64 @@
+#!/usr/bin/node
+
+$(document).ready(function () {
+  let amenity_ids = {};
+  $("input[type='checkbox']").change(function () {
+    if (this.checked) {
+      amenity_ids[$(this).attr("data-id")] = $(this).attr("data-name");
+    } else {
+      delete amenity_ids[$(this).attr("data-id")];
+    }
+    const amenities = [];
+    for (const k in amenity_ids) {
+      amenities.push(amenity_ids[k]);
+    }
+    if (amenities.length > 0) {
+      $(".amenities h4").text(amenities);
+    } else {
+      $(".amenities h4").html('&nbsp;');
+    }
+  });
+
+  $.get('http://127.0.0.1:5001/api/v1/status/', function(data, textStatus) {
+    if (data.status == 'OK') {
+      $('div#api_status').addClass('available');
+    } else {
+      $('div#api_status').removeClass('available');
+    }
+  });
+  function postPlaces (url, data) {
+    $.post({
+      url: url,
+      headers: {'Content-Type': 'application/json'},
+      data: data,
+      dataType: 'json',
+      success: function(data) {
+        $.each(data, function (index, place) {
+          const article = $('<article></article>');
+	  article.html(
+	    `<div class="title_box">
+	       <h2>${place.name}</h2>
+	       <div class="price_by_night">${place.price_by_night }</div>
+	     </div>
+	     <div class="information">
+	       <div class="max_guest">${place.max_guest} Guest${place.max_guest !== 1 ? 's' : ''}</div>
+               <div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms !== 1 ? 's' : ''}</div>
+               <div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? 's' : ''}</div>
+	     </div>
+             <div class="description">
+	       ${place.description}
+             </div>
+	   `);
+	  $('section.places').append(article);
+        });
+      }
+    });
+  }
+  postPlaces("http://127.0.0.1:5001/api/v1/places_search", JSON.stringify({}));
+  $('button').click(function () {
+    const data = Array.from($(':checkbox:checked')).map(amenity => amenity.dataset.id);
+    const body = JSON.stringify({amenities: data});
+    postPlaces("http://127.0.0.1:5001/api/v1/places_search", body);
+  })
+  //$('button').click( postPlaces("http://127.0.0.1:5001/api/v1/places_search", am));
+});
